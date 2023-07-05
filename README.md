c13edb2715517f0137e8274eb7b284ad744b1e5f
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2023-07-05 14:54:53 +0200

    [Update] Benchmark actions service working in Nest Js, test Grpc call with postman

---
+++ README.md
@@ -0,0 +1,10 @@
+# phalanger-app
+Projets dans le cadre du Hackathon des masters 2 IW à l'ESGI
+
+### 28/06 Modification de l'architecture du Projet pour passer en MicroServices :
+
+Ma base de donnée PostGres est déjà Dockerisé il me reste à faire pour passer en microservices :
+
+* conteneuriser dans Docker l'api et le front de l'application.
+* créer un service d'authentification communicant en GRPC.
+
---
+++ svelte-client/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+
+<script>
+    import {Card, Button, GradientButton, ButtonGroup} from "flowbite-svelte";
+</script>
+<Card>
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">NestJs Bun</h5>
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+        Get datas from Bun environnement
+    </p>
+    <ButtonGroup class="space-x-px">
+        <GradientButton color="purpleToBlue">Profile</GradientButton>
+        <GradientButton color="cyanToBlue">Settings</GradientButton>
+        <GradientButton color="greenToBlue">Messages</GradientButton>
+    </ButtonGroup>
+</Card>

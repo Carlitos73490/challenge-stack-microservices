bcfbfde97bde42ec72e0380d888f76ea4ef42674
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2023-07-06 14:59:28 +0200

    [Update] SvelteKit form action to Get GRPC response time

--- svelte-client/src/routes/+page.server.ts
+++ svelte-client/src/routes/+page.server.ts
@@ -6,12 +6,28 @@ import {ChannelCredentials} from "@grpc/grpc-js";
 /** @type {import('./$types').PageLoad} */
 export async function load() {
 
-    // setup a transport and create a client instance
-    let transport = new GrpcTransport({
-          channelCredentials: ChannelCredentials.createInsecure(),
-        host: "localhost:5000"
-    });
-    let client = new BenchmarkActionsServiceClient(transport);
-    let {response} = await client.bigArrayProcess({arraySize: 9999});
-    return {elapsedTime : response.elapsedTime}
+
 }
+
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+    bigArrayProcess:  async ({ cookies, request } : any)  => {
+        const data = await request.formData();
+        const size = parseInt(data.get('size'));
+        const port = data.get('port');
+
+        // setup a transport and create a client instance
+        let transport = new GrpcTransport({
+            channelCredentials: ChannelCredentials.createInsecure(),
+            host: `localhost:${port}`
+        });
+        let client = new BenchmarkActionsServiceClient(transport);
+        let {response} = await client.bigArrayProcess({arraySize: size});
+
+        return {elapsedTime : response.elapsedTime, success: true };
+    },
+    bigDbFetch: async (event : any) => {
+        // TODO register the user
+    }
+};
--- svelte-client/src/routes/+page.svelte
+++ svelte-client/src/routes/+page.svelte
@@ -1,27 +1,31 @@
 
 <script>
     import {Card, Button, GradientButton, ButtonGroup, Label, Input, Textarea} from "flowbite-svelte";
-    /** @type {import('./$types').PageData} */
-    export let data;
 
-
-    let arraySize = 1000
-    let output = data
-    console.log(data)
-
+    let defaultPort = 5000
+    let defaultArraySize = 10000
+    /** @type {import('./$types').ActionData} */
+    export let form;
 </script>
 <Card>
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">NestJs Bun</h5>
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
         Get datas from Bun environnement
     </p>
-    <div>
-        <Label for="array_size" class="mb-2" >Array Size</Label>
-        <Input type="number" id="array_size" placeholder="10000" bind:value={arraySize} required  />
-    </div>
-    <ButtonGroup class="space-x-px">
-        <GradientButton color="purpleToBlue" >BigArray</GradientButton>
-        <GradientButton color="cyanToBlue">BigDbFetch</GradientButton>
-    </ButtonGroup>
-    <Textarea id="textarea-id" placeholder="Results" rows="4" name="message" bind:value={output}/>
+
+
+
+    <form method="POST" >
+        <Label for="size" class="mb-2" >Array Size</Label>
+        <Input type="number" id="size" name="size" placeholder="10000" bind:value={defaultArraySize} required  />
+        <Label for="size" class="mb-2" >Port</Label>
+        <Input type="number" id="port" name="port" placeholder="5000" bind:value={defaultPort} required  />
+
+        <button color="purpleToBlue" formaction="?/bigArrayProcess" >Launch GRPC Request</button>
+    </form>
+    {#if form?.success}
+        <!-- this message is ephemeral; it exists because the page was rendered in
+               response to a form submission. it will vanish if the user reloads -->
+        <Textarea id="textarea-id" placeholder="Results" rows="4" name="message" bind:value={form.elapsedTime}/>
+    {/if}
 </Card>
